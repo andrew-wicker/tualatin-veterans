4e7bb60d4c7f2c372459879a52e0a71891dca163
Andrew <apwicker@gmail.com>
2024-04-25 22:08:34 -0700

    scaffold front end, remove extraneous code, add title and image

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img
+        style={{ width: '25%', height: 'auto' }}
+        src='../public/Logotest.png'
+      />
+      <h2>Tualatin Veterans Group</h2>
+    </div>
+  );
+}
+
+export default App;
