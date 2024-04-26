71138d3c010a5e3bf5fd98178eb54e0b5410d364
Andrew <apwicker@gmail.com>
2024-04-26 13:32:03 -0700

    shadcn ui styling, additional layout adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
-// import MemberForm from './components/MemberDataForm';
 import MemberForm from './components/MemberForm';
+import { Toaster } from './components/ui/toaster';
+import { Separator } from './components/ui/separator';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <img
-        style={{ width: '25%', height: 'auto' }}
-        src='../public/Logotest.png'
-      />
-      <h1>Tualatin Veterans Group</h1>
-      <MemberForm />
+    <div className="bg-gray-50 min-h-screen p-6">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center justify-center mb-6">
+          <img
+            style={{ width: '20%', height: 'auto' }}
+            src="../public/tualatin-historical-society.png"
+          />
+          <h1 className="text-3xl font-bold ml-4">Tualatin Veterans Group</h1>
+        </div>
+
+        <div className="w-full max-w-3xl bg-white rounded-lg shadow-lg p-8">
+          <div className="mb-6">
+            <h1 className="text-2xl font-bold mb-4">Member Form</h1>
+          </div>
+          <Separator />
+          <MemberForm />
+        </div>
+      </div>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/components/MemberForm.tsx
+++ frontend/src/components/MemberForm.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { Button } from './ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { useToast } from '@/components/ui/use-toast';
+
+// interface FieldProps {
+//   firstName: string;
+//   middleName: string;
+// }
 
 const formSchema = z.object({
   firstName: z.string().min(2).max(50),
@@ -17,10 +34,150 @@ const formSchema = z.object({
   emailAddress: z.string().email(),
 });
 
+type FormFieldName = keyof z.infer<typeof formSchema>;
+
+const formFields: {
+  name: FormFieldName;
+  label: string;
+  placeholder: string;
+  description: string;
+}[] = [
+  {
+    name: 'firstName',
+    label: 'First Name',
+    placeholder: 'First Name',
+    description: '',
+  },
+  {
+    name: 'middleName',
+    label: 'Middle Name',
+    placeholder: 'Middle Name',
+    description: '',
+  },
+  {
+    name: 'lastName',
+    label: 'Last Name',
+    placeholder: 'Last Name',
+    description: '',
+  },
+  {
+    name: 'dateOfBirth',
+    label: 'Date of Birth',
+    placeholder: 'Date of Birth',
+    description: '',
+  },
+  {
+    name: 'streetAddress',
+    label: 'Address',
+    placeholder: 'Address',
+    description: '',
+  },
+  {
+    name: 'streetAddress2',
+    label: 'Address 2',
+    placeholder: 'Address 2',
+    description: '',
+  },
+  {
+    name: 'city',
+    label: 'City',
+    placeholder: 'City',
+    description: '',
+  },
+  {
+    name: 'state',
+    label: 'State',
+    placeholder: 'State',
+    description: '',
+  },
+  {
+    name: 'zipCode',
+    label: 'Zip Code',
+    placeholder: 'Zip Code',
+    description: '',
+  },
+  {
+    name: 'phoneNumber',
+    label: 'Phone',
+    placeholder: 'Phone',
+    description: '',
+  },
+  {
+    name: 'emailAddress',
+    label: 'Email',
+    placeholder: 'Email',
+    description: '',
+  },
+];
+
 function MemberForm() {
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: '',
+      middleName: '',
+      lastName: '',
+      dateOfBirth: '',
+      streetAddress: '',
+      streetAddress2: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      phoneNumber: '',
+      emailAddress: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <div>
-      <h3>Member form</h3>
+    <div className="p-6 bg-white rounded-lg shadow-md">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          {formFields.map((fieldItem) => (
+            <FormField
+              key={fieldItem.name}
+              control={form.control}
+              name={fieldItem.name}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{fieldItem.label}</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder={fieldItem.placeholder}
+                      {...field}
+                      className="border-gray-300 rounded"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-gray-600">
+                    {fieldItem.description}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button
+            type="submit"
+            className="bg-gray-500 text-white px-4 py-2 rounded"
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
